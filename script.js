8659c0d94089c9f408ac050d52f077d5189f4c5d
Evgeniy Strelets <jenyastrelets@gmail.com>
2021-10-09 14:29:03 +0300

    add min and max allowed values
    Added minimum and maximum values ​​to the project. Created special styles for error output.
    Updated the mechanics of derivation of numbers and added an Error to js file

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ const firstNum = document.querySelector('[data-number1]'),
 function generateRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 btn.addEventListener('click', () => {
     const a = firstNum.value,
           b = secondNum.value;
 
-    result.textContent = `${generateRandomNumber(a, b)}`;
+    if (a < -999999 || a > 999999 ) {
+        result.classList.add('error')
+        return result.textContent = 'Недопустимое первое значение';
+    } else if (b < -999999 || b > 999999 ) {
+        result.classList.add('error')
+        return result.textContent = 'Недопустимое последнее значениие   ';
+    }
+
+    return result.textContent = `${generateRandomNumber(a, b)}`;
 });
